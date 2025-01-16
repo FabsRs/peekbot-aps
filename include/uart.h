974f22ff5364d748a90753787875e615a83c8331
Fabrizio Eduardo Rojas Iacocca <fabriziorojas2000@gmail.com>
2025-01-16 09:33:02 -0500

    General - Added rule of 10 to README - Created UART.h and YMODEM.h - Created Main.c

---
+++ include/uart.h
@@ -0,0 +1,6 @@
+#ifndef UART_H
+#define UART_H
+
+//TODO: Read ATMEGA328P documentation and define UART
+
+#endif//UART_H
---
+++ include/ymodem.h
@@ -0,0 +1,6 @@
+#ifndef YMODEM_H
+#define YMODEM_H
+
+// TODO: Read Ymodem implementation and se how to include it in the project
+
+#endif//YMODEM_H

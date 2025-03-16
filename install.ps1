6d8f30ca85f13b6e20f635b0e57fe39d3ac617e5
Fabrizio Eduardo Rojas Iacocca <fabriziorojas2000@gmail.com>
2025-03-15 22:11:23 -0400

    - changed port from COM10 to COM11

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $AVRDUDE = 'C:\Users\Admin\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude.exe'
 $CONF = 'C:\Users\Admin\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf'
 
-$COM = 'COM10'
+$COM = 'COM11'
 
 $FLAGS = @('-v';'-V';'-patmega328p';'-carduino';'-b115200';'-D';)
 
